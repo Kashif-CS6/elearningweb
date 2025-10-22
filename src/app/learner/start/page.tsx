e1a07880227618fafbfa665e429400b2d1939cf9
Kashif-CS6 <kashifnabeel.cs@gmail.com>
2025-10-22 11:33:53 +0500

    pushing code of responsivness

--- src/app/learner/start/page.tsx
+++ src/app/learner/start/page.tsx
@@ -8,12 +8,12 @@ export default function OnboardingApp() {
 
   return (
     <div className="h-screen bg-gradient-to-br from-gray-50 to-gray-100 flex items-center justify-center lg:p-6">
-      <div className="bg-white rounded-[6px] shadow-sm flex flex-col  items-center justify-between py-20  xl:w-[1130px] h-[796px]  text-center">
+      <div className="bg-white rounded-[6px] shadow-sm flex flex-col  items-center justify-between py-20  xl:w-[1130px] md:h-[796px]  text-center">
         <div>
-          <h1 className="text-[24px] lg:text-[30px] leading-[40px]  text-gray-700 font-[500] mb-2">
+          <h1 className="text-[20px] md:text-[24px] lg:text-[30px] leading-[40px]  text-gray-700 font-[500] mb-2">
             This is your magic backpack.
           </h1>
-          <p className="text-[24px] lg:text-[30px] leading-[40px]  text-gray-700 font-[500] mb-2 px-2 md:px-10">
+          <p className="text-[20px] md:text-[24px] lg:text-[30px] leading-[40px]  text-gray-700 font-[500] mb-2 px-2 md:px-10">
             Let{"'"}s pack it with your dreams and secrets so I can know you
             better!
           </p>
@@ -26,6 +26,7 @@ export default function OnboardingApp() {
               width={184.25}
               height={242.88}
               alt="ai icon"
+              className="w-[100px] h-[100px] md:w-[184.25px] md:h-[242.88px]"
             />
           </div>
         </div>
